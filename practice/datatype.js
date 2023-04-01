9658874a65fa20f044378fa643e640374f22dd00
amancoderz <464aman@gmail.com>
2023-04-02 01:43:59 +0530

    file 1&2

---
+++ practice/datatype.js
@@ -0,0 +1,20 @@
+//premetive datatype
+//nn bb ss u
+let a= null;
+let b= 123;
+let c= true;
+let d= BigInt("34");
+let e= "aman";
+let f= Symbol("qwq");
+let g;
+console.log(a,b,c,d,e,f,g);
+
+//Non premetive data type (object in js)
+const item ={
+    "aman":true,
+    "huma":false,
+    "faheem":75,
+    "Ali": undefined
+}
+item['shnwz']="477"
+console.log(item)
---
+++ practice/variable.js
@@ -0,0 +1,14 @@
+ // variable in javascript
+// var it is declare as globally. 
+// and it can updated and redeclared. 
+// in the present time var cannot be used.
+
+// let is also a variable declaration 
+// it declare in block
+// it is updated but not this is not redeclared
+
+// const cant be redeclared and updated
+// const is fixed
+let a=5;
+console.log(a);
+
